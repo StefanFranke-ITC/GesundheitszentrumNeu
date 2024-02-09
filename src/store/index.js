e4eb84da4aba7d5d1b6d51dd7e90fa87cb29b730
Leandro Graf <Leandro.Graf@quinscape.de>
2024-02-09 20:54:16 +0100

    deploy

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import {createStore} from 'vuex'
 
 export default createStore({
     state: {
-        user: true,
+        user: false,
         contentDelta: '',
         preiseArray: [],
         videoArray: [],
