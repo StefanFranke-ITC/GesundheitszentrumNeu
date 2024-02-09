ea2cfe25864afad9d0915822f2de97b98c2e0b4f
Leandro Graf <Leandro.Graf@quinscape.de>
2024-02-09 20:40:09 +0100

    deploy

--- src/views/VerwaltungsView.vue
+++ src/views/VerwaltungsView.vue
@@ -64,7 +64,7 @@ export default {
     logout() {
       localStorage.removeItem('token');
       this.$store.dispatch('user', null);
-      this.$router.push('/');
+      location.reload()
     },
   },
   mounted() {
