e728dcf4cfd11c75d1a8e736d8f062d0304b964c
Leandro Graf <Leandro.Graf@quinscape.de>
2024-02-12 22:37:42 +0100

    headers

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -93,6 +93,7 @@ export default {
             });
         await localStorage.setItem('token', response.data.token)
         this.$store.state.user = response.data.user
+        console.log(this.error)
       } catch (error) {
 
         if (error === 'AxiosError: Request failed with status code 401') {
